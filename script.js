595ea6f1aea4aaee83c42de7bb66e4adac8c8b18
K3vin1v4 <kevin.pauluk@escola.pr.gov.br>
2026-04-06 08:51:26 -0300

    script

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+const secaoAgenda = document.querySelector("#agenda");
+if (secaoAgenda){
+const ulAgenda = secaoAgenda.querySelector("ul");
+if (ulAgenda){
+    ulAgenda.classList.add("agenda")
+     }
+   }
 });
